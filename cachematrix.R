77e4cb19d578cf680e756eda98195747ecfd8de5
Ayman A. Amin <aymanaamin@gmail.com>
2020-07-21 18:18:32 +0200

    Functions for Caching the Inverse of a Matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this small project we have a pair of functions: makeCacheMatrix and cacheSolve 
+## that cache the inverse of a square invertible matrix, rather than computing it repeatedly.
+## Note: the functions assume the matrix is inverible square.
 
-## Write a short comment describing this function
+library(matrixcalc) # required library to check whether the matrix is square.
 
+## makeCacheMatrix: This function creates a special  -inverible square- "matrix" object
+# that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+      Inv <- NULL
+      
+      setMatrix <- function(y){
+            x <<- y
+            Inv <<- NULL
+      }
+      getMatrix <- function() x 
+      
+      setMatrixInv <- function(inverse) Inv <<- inverse
+      getMatrixInv <- function() Inv 
+      list=c(setMatrix = setMatrix,
+             getMatrix = getMatrix,
+             setMatrixInv = setMatrixInv,
+             getMatrixInv = getMatrixInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special -inverible square- "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), then 
+# cacheSolve should retrieve the inverse from the cache, 
+# and if the matrix is singular or non-square, then 
+# the inverse is not calculated and cacheSolve return warning message.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      Inv <- x$getMatrixInv()
+      
+      if (!is.null(Inv)){
+            message("getting cached Matrix Inverse")
+            return(Inv)
+      }
+      mat <- x$getMatrix()
+      if (is.square.matrix(mat)==T && det(mat) != 0 ) {
+            Inv <- solve(mat)
+            x$setMatrixInv(Inv)
+            return(Inv)} else { print("Warning: Your matrix is singular or non-square!") }
 }
+
+
+#=== examples
+# 1. inverible square matrix:
+x = makeCacheMatrix(matrix(1:4,2,2))
+cacheSolve(x)
+
+# 2. singlar square matrix:
+x = makeCacheMatrix(matrix(c(1,1,1,1),2,2))
+cacheSolve(x)
+
+# 3. Non-square matrix:
+x = makeCacheMatrix(matrix(1:6,2,3))
+cacheSolve(x)
+
